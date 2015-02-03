2476c7469e771196aa6b7a83680cb0cb49df99a2
Jeff Stafford <jeff.stafford@live.com>
2015-02-02 19:41:13 -0800

    works, but massive issues still...

--- larva_tracker.m
+++ larva_tracker.m
@@ -68,8 +68,7 @@ for nofr = 1:nfrm_movie
     frame = frame - background;
     
     % Threshold image.
-    thresh = graythresh(frame) + 0.025; % otsu's method
-    %thresh = 0.25;
+    thresh = graythresh(frame) + 0.025; % otsu's method + an offset
     frame_thresh = medfilt2(im2bw(frame, thresh));
     
     % Check to see if the threshold is too low, recalculate frame with
@@ -97,7 +96,7 @@ for nofr = 1:nfrm_movie
     
     % label regions and compute region properties
     binaryLabel(:,:,nofr) = bwlabel(binaryMap(:,:,nofr),4);
-    regions = regionprops(binaryLabel(:,:,nofr),'Area', 'Centroid', 'PixelIdxList', 'PixelList');
+    regions = regionprops(binaryLabel(:,:,nofr),'Area', 'Centroid', 'PixelList');
     
     % small regions (not larvae) are filtered out of our array
     regionNum = 1;
@@ -111,66 +110,118 @@ for nofr = 1:nfrm_movie
     end
     
     % relabel regions
+    regionsLength = length([regions.Area]);
     if (nofr == 1)
-        % sort and label regions by euclidean distance to (0,0)
-        for i = 1:length([regions.Area])
+        % sort regions by euclidean distance to (0,0)
+        for i = 1:regionsLength
             regions(i).DistToOrigin = sqrt(regions(i).Centroid(1)^2 + regions(i).Centroid(2)^2);
         end
-        [tmp, idx] = sort([regions.DistToOrigin]);
+        [temp, idx] = sort([regions.DistToOrigin]);
         regions = regions(idx);
-        for newLabel = 1:length([regions.Area])
-            % change the value of every labeled pixel to the new value
-            for pixRow = 1:size(regions(newLabel).PixelList,1);
-                binaryLabel( ...
-                    regions(newLabel).PixelList(pixRow,1), ...
-                    regions(newLabel).PixelList(pixRow,2), nofr) ...
-                    = newLabel;
+    else
+        % sort regions based on proximity to last labeled regions
+        for i = 1:regionsLength
+            % create a vector of dists, ordered from 1:(all of lastRegion's regions)
+            dist = zeros(1,length([lastRegions.Area]));
+            for j = 1:length([lastRegions.Area])
+                dist(j) = pdist2(regions(i).Centroid, lastRegions(j).Centroid);
             end
+            % which region centroid matches the last's most closely?
+            [tmp, idx] = min(dist);
+            regions(i).lastLabelDists = idx;
         end
-    else
-        % relabel regions based on proximity to last labeled regions
+        [temp, idx] = sort([regions.lastLabelDists]);
+        regions = regions(idx);
     end
     
-    checkNum = length([regions.Area]);
-    if checkNum < numLarvae % if missing larvae
-        % TODO find regions larger than larva are supposed to be
-        % count those regions twice or subtract last frame
-        position(nofr,:) = NaN; %placeholder
-    elseif checkNum > numLarvae % we've likely picked up noise
-        % TODO pick the areas closest to last size and position
-        position(nofr,:) = NaN; %placeholder!
-    else
-        % Need to sort by proximity to origin, and check for proximity to
-        % last frame, THEN extract centroids
-        centroid = [regions.Centroid]';
-        for larva = 1:checkNum
-            position(nofr, (larva*2-1):(larva*2)) = centroid((larva*2-1):(larva*2));
+    % change the value of every labeled pixel to the new value (based on
+    % sort order)
+    for newLabel = 1:regionsLength
+        for pixRow = 1:size(regions(newLabel).PixelList,1);
+            binaryLabel( ...
+                regions(newLabel).PixelList(pixRow,2), ...
+                regions(newLabel).PixelList(pixRow,1), nofr) ...
+                = newLabel;
+            % might have indexed this wrong!!!!!!!!! CHECK ME!!!!!
         end
     end
     
-    % store regionProps data for next loop iteration
+%     if regionsLength < numLarvae % if missing larvae
+%         % TODO find regions larger than larva are supposed to be
+%         % count those regions twice or subtract last frame
+%         position(nofr,:) = NaN; %placeholder
+%     elseif regionsLength > numLarvae % we've likely picked up noise
+%         % TODO pick the areas closest to last size and position
+%         % areas furthest from last size should be last in our sorted
+%         % regions
+%         position(nofr,:) = NaN; %placeholder!
+%     else
+%         % Need to sort by proximity to origin, and check for proximity to
+%         % last frame, THEN extract centroids
+%         centroid = [regions.Centroid]';
+%         for larva = 1:checkNum
+%             position(nofr, (larva*2-1):(larva*2)) = centroid((larva*2-1):(larva*2));
+%         end
+%     end
+    
+    % store 'regions' regionProps data for next loop iteration
     lastRegions = regions;
 end
 close(waitDialog);
 
 %% create a video for debugging
 
-% reformat data
-videoOut = uint8(binaryMap);
-videoOut = videoOut*255;
+% generate colors
+% maximum = max(binaryLabel(:));
+maximum = max(test(:));
+colorMap = zeros(maximum, 3);
+for i = 1:maximum
+    val = double(i)/double(maximum);
+    % r channel
+    colorMap(i,1) = ceil(255*val);
+    % g channel
+    if val < 0.5
+        colorMap(i,2) = ceil(255*val*2);
+    else
+        colorMap(i,2) = 255 - ceil(255*(val-0.5)*2);
+    end
+    % b channel
+    colorMap(i,3) = 255 - ceil(255*val);
+end
+% randomize colormap for more variety
+randomIdx = randperm(maximum);
+colorMap = colorMap(randomIdx, :);
 
-writer = VideoWriter('larvaDebug_ellenTest_largeROI.avi');
+% add color data to show labeling
+blSize = size(binaryLabel);
+movieFrames = zeros(blSize(1), blSize(2), 3, blSize(3), 'uint8');
+waitDialog = waitbar(0, 'Creating video...');
+for nofr = 1:blSize(3)
+    waitbar(nofr/nfrm_movie, waitDialog, ...
+        strcat({'Creating frame'},{' '}, num2str(nofr), {' '}, {'of'}, {' '}, num2str(nfrm_movie)));
+    for channel = 1:3
+        movieFrames(:,:,channel,nofr) = binaryLabel(:,:,nofr);
+        %testFrame(:,:,channel) = test;
+        for color = 1:maximum
+            % next line needs an indexing fix
+            tmp = movieFrames(:,:,channel,nofr);
+            tmp(tmp == color) = colorMap(color,channel);
+            movieFrames(:,:,channel,nofr) = tmp;
+            
+%             tmp = testFrame(:,:,channel);
+%             tmp(tmp == color) = colorMap(color,channel);
+%             testFrame(:,:,channel) = tmp;
+        end
+    end
+end
+
+writer = VideoWriter('larvaDebug_tracking.avi');
 writer.FrameRate = vr.FrameRate;
 open(writer);
-preFrame = zeros(ROI(4)+1, ROI(3)+1, 'uint8');
-waitDialog = waitbar(0, 'Creating video...');
 for nofr = 1:nfrm_movie
     waitbar(nofr/nfrm_movie, waitDialog, ...
         strcat({'Writing frame'},{' '}, num2str(nofr), {' '}, {'of'}, {' '}, num2str(nfrm_movie)));
-    for channel = 1:3
-        preFrame(:,:,channel) = videoOut(:,:,nofr);
-    end
-    writeVideo(writer, im2frame(preFrame));
+    writeVideo(writer, im2frame(movieFrames(:,:,:,nofr)));
     
 end
 close(writer);
